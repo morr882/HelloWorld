ea8ec17041ce2fa9776d7e479c3a612d71ea037a
morr882 <morr@royalroad.school.nz>
2019-12-20 13:16:47 +1300

    Added new branch

--- MyFirstProject/src/HelloWorldExample.java
+++ MyFirstProject/src/HelloWorldExample.java
@@ -2,7 +2,7 @@
 public class HelloWorldExample {
 
 	public static void main(String[] args) {
-		// Output Hello World to the user
+		// Added to new BranchExample
 		 System.out.println("Hello World");
 	}
 
